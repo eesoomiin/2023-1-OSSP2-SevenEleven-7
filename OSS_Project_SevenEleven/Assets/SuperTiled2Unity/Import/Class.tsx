37ecd4bd7fb40fcb271fe5ea575db82317e41b54
수민 <127471190+eesoomiin@users.noreply.github.com>
2023-05-08 23:14:42 +0900

    test again

---
+++ OSS_Project_SevenEleven/Assets/SuperTiled2Unity/Import/Class.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Class" tilewidth="4" tileheight="4" tilecount="30625" columns="175">
+ <image source="Class.png" width="700" height="700"/>
+</tileset>
---
+++ OSS_Project_SevenEleven/Assets/SuperTiled2Unity/Import/LibraryA.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="LibraryA" tilewidth="4" tileheight="4" tilecount="30100" columns="172">
+ <image source="LibraryA.png" width="691" height="700"/>
+</tileset>
